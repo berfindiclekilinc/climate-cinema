fcc5404d226dbb6290176948a4415ee0f22ce4da
berfindiclekilinc <74237491+berfindiclekilinc@users.noreply.github.com>
2024-02-13 14:40:49 +0300

    first commit 🩷

---
+++ src/main/java/berfin/climatecinema/ClimateCinemaApplication.java
@@ -0,0 +1,13 @@
+package berfin.climatecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimateCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/berfin/climatecinema/ClimateCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package berfin.climatecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
